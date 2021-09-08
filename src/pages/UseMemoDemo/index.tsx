f660c88e6e6b48990764e4874dd9d08cabf7c819
baiyun <baiyun.feewee.cn>
2021-09-08 10:20:13 +0800

    usememo优化

--- src/pages/UseMemoDemo/index.tsx
+++ src/pages/UseMemoDemo/index.tsx
@@ -1,31 +1,41 @@
 import React, {useEffect, useMemo, useState} from "react";
-import List from "./components/List";
 import moment from 'moment';
-
+/**************************封印开始********************************/
+interface ListProps {
+  list: number[]
+}
+export function List({list}: ListProps) {
+  console.log('list', moment().format("YYYY-MM-DD HH:mm:ss"));
+  return (
+    <div>
+      {list.map(i => <div key={i}>{i}</div>)}
+    </div>
+  );
+}
+/**************************封印结束********************************/
 export default function Index() {
-  const [timer, setTimer] = useState(0);
+  const [timer, setTimer] = useState(moment());
   const [list] = useState([1, 2, 3, 4, 5, 6, 7]);
   // const list = [1, 2, 3, 4, 5, 6, 7];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimer(moment().valueOf());
+      setTimer(moment());
     }, 1000);
-
     return () => {
       interval && clearInterval(interval);
     };
   }, []);
 
-  console.log('i', timer);
-
   const lst = useMemo(() => {
     return (<List list={list} />);
   }, [list]);
 
+  console.log('timer', timer.format('YYYY-MM-DD HH:mm:ss'));
+
   return (
     <div>
-      <span>{moment(timer).format('YYYY-MM-DD HH:mm:ss')}</span>
+      <span>{timer.format('YYYY-MM-DD HH:mm:ss')}</span>
       {lst}
     </div>
   );
